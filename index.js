e6f0fd0b09a4f223cf459135beb7e0fb1ab22dee
Jericho1611 <joshmg426@gmail.com>
2024-07-02 22:38:28 -0400

    Creating and Uploading files

---
+++ index.js
@@ -0,0 +1,80 @@
+import fs from 'fs';
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import figlet from 'figlet';
+
+const generateREADME = (answers) => {
+  return `
+# ${answers.projectName}
+
+## Description
+${answers.description}
+
+## Installation
+\`\`\`
+${answers.installation}
+\`\`\`
+
+## Usage
+\`\`\`
+${answers.usage}
+\`\`\`
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## More
+You can also find more of my work at [${answers.github}](https://github.com/${answers.github}).
+`;
+};
+
+const promptUser = async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'projectName',
+      message: '📌 Project Name:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: '📕 Project Description:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage instructions:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: '👤 GitHub username:',
+    },
+  ]);
+
+  return answers;
+};
+
+const main = async () => {
+  console.log(chalk.redBright(figlet.textSync('README.md Generator')));
+  
+  const answers = await promptUser();
+  const readmeContent = generateREADME(answers);
+
+  fs.writeFileSync('README.md', readmeContent);
+
+  console.log(chalk.green('README.md has been generated successfully!'));
+};
+
+main();
